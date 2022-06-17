ccbb284cf1c3a48d74c17fa046f6b2b517cd04b2
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2022-06-17 18:53:55 +0300

    <Final commit>

---
+++ Shopping Spree/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopping_Spree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private readonly List<Product> products;
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.products = new List<Product>();
+        }
+
+        public IReadOnlyCollection<Product> ProductsCollection
+            => this.products;
+
+        public string Name
+        {
+            get 
+            {
+                return name;
+            }
+            private set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get
+            {
+                return money; 
+            }
+            private set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+
+        public bool AddProduct(Product product)
+        {
+            if (this.Money -product.Cost < 0)
+            {
+                return false;
+            }
+            products.Add(product);
+            this.Money -= product.Cost;
+
+            return true;
+        }
+    }
+}
---
+++ Shopping Spree/StartUp.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shopping_Spree
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Person> personKvp = new Dictionary<string, Person>();
+
+            Dictionary<string, Product> productKvp = new Dictionary<string, Product>();
+
+
+            try
+            {
+                string[] people = Console.ReadLine()
+                    .Split(new char[] { '=', ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < people.Length; i += 2)
+                {
+                    string name = people[i];
+                    decimal money = decimal.Parse(people[i + 1]);
+
+                    Person person = new Person(name, money);
+
+                    personKvp.Add(name, person);
+                }
+
+
+                string[] products = Console.ReadLine()
+                    .Split(new char[] { '=', ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < products.Length; i += 2)
+                {
+                    string name = products[i];
+                    decimal cost = decimal.Parse(products[i + 1]);
+
+                    Product product = new Product(name, cost);
+
+                    productKvp.Add(name, product);
+                }
+
+
+                string command = Console.ReadLine();
+
+                while (command != "END")
+                {
+                    string[] commandInfo = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                    string personName = commandInfo[0];
+                    string productName = commandInfo[1];
+
+                    Person person = personKvp[personName];
+                    Product product = productKvp[productName];
+
+                    bool isAdded = person.AddProduct(product);
+
+                    if (!isAdded)
+                    {
+                        Console.WriteLine($"{personName} can't afford {productName}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{personName} bought {productName}");
+                    }
+                    command = Console.ReadLine();
+                }
+
+                foreach (var (name , person) in personKvp)
+                {
+                    string productsInfo = person.ProductsCollection.Count > 0
+                        ? string.Join(", ", person.ProductsCollection.Select(x => x.Name))
+                        : "Nothing bought";
+
+                    Console.WriteLine($"{name} - {productsInfo}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
